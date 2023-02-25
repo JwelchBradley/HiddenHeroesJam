470d86d6d33141460c1eb65f9abf004a77929df0
jwelc <jwelch@mail.bradley.edu>
2023-02-24 21:15:12 -0600

    Player Movement Updates

--- Hidden Heroes Game Jam/Assets/Scripts/Player/PlayerMovement.cs
+++ Hidden Heroes Game Jam/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,8 +9,20 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rigidbody;
     private Transform cameraTransform;
 
+    private float currentGravity = 0.0f;
+
     [SerializeField] private float playerSpeed = 7.5f;
+    [SerializeField] private float maxSlope = 60.0f;
+    [SerializeField] private float jumpVelocity = 10.0f;
+
+    [SerializeField] private float extraGravity = 10.0f;
+
     [SerializeField] private Transform movementTransform;
+    [SerializeField] private SphereCollider groundCheckCollider;
+
+
+    private Vector3 normal;
+    private List<GameObject> contactObjects = new List<GameObject>(); 
     #endregion
 
     #region Functions
@@ -23,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+        Jump();
     }
 
     private void MovePlayer()
@@ -32,7 +46,61 @@ public class PlayerMovement : MonoBehaviour
         var xMove = Input.GetAxisRaw("Horizontal");
         var yMove = Input.GetAxisRaw("Vertical");
 
-        rigidbody.velocity = (movementTransform.transform.forward * yMove + movementTransform.transform.right * xMove) * playerSpeed;
+        if (contactObjects.Count != 0)
+        {
+            var vel = (movementTransform.transform.forward * yMove + movementTransform.transform.right * xMove).normalized;
+            var planeMovement = Vector3.ProjectOnPlane(vel, normal).normalized * playerSpeed;
+            rigidbody.velocity = planeMovement;
+        }
+        else
+        {
+            var vel = (movementTransform.transform.forward * yMove + movementTransform.transform.right * xMove).normalized * playerSpeed;
+            vel.y = rigidbody.velocity.y - extraGravity*Time.fixedDeltaTime;
+            rigidbody.velocity = vel;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Collider[] colliders = Physics.OverlapSphere(groundCheckCollider.transform.position, groundCheckCollider.radius);
+
+            foreach(Collider col in colliders)
+            {
+                if (!col.gameObject.CompareTag("Player"))
+                {
+                    print(col.gameObject.tag);
+                    var vel = rigidbody.velocity;
+                    vel.y = jumpVelocity;
+                    rigidbody.velocity = vel;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        contactObjects.Add(collision.gameObject);
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (Vector3.Angle(contact.normal, Vector3.up) < maxSlope)
+            {
+                normal = contact.normal;
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        contactObjects.Remove(collision.gameObject);
+
+        if (contactObjects.Count == 0)
+        {
+            normal = Vector3.zero;
+        }
     }
     #endregion
 }
